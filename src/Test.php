215ac8faa74f8825e1811d6649fecee4c56857dc
苏楠 <779212786@qq.com>
2020-11-27 10:15:52 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,15 @@
+<?php
+namespace sntools;
+
+/**
+ * @author : SN
+ * @time   : 2020/11/27 10:12
+ * @des    :
+ */
+class Test
+{
+    public function test(){
+        echo "sntools test..";
+    }
+
+}
